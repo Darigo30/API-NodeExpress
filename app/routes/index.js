86e6df0fefdcbebd166c250b2a9dce40420eaeba
darigo30 <darigomez30@gmail.com>
2022-06-30 13:25:28 -0400

    nuevos cambios en index con params y cambios en detalle

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const axios = require('axios').default
+
  
 //Ruta para obtener el resultado de la búsqueda
 
@@ -8,7 +9,7 @@ router.get('/api/items', async (req, res) => {
     const search = req.query.q;
     const url = `https://api.mercadolibre.com/sites/MLA/search`
     try {
-        const response = await axios.get(url, {params: { q: search} })
+        const response = await axios.get(url, {params: { q: search } })
         const data = response.data
         res.json(data)
     }
